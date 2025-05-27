51fdcf90d928083e6bd865d594607ca1bb834aff
viathree <viathree01@gmail.com>
2025-05-27 16:20:19 +0700

    CV

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -7,10 +7,19 @@ export function TimelineDemo() {
       title: "2025",
       content: (
         <div>
+            <h1 className="text-2xl mb-8 font-bold text-[var(--color-text)] dark:text-[var(--color-text)]">
+              IT Support Developer - PT.Megaputra <br></br> Web Content Uploading - Gaotek Inc
+          </h1>
           <p
-            className="mb-8 text-md font-normal text-[var(--color-text)]md:text-lg dark:text-[var(--color-text)]">
-           I have experience in IT support developer at PT.Megaputra and Web Content Uploading at Gaotek Inc remotely.
+            className="mb-8 text-md font-normal text-[var(--color-text)] md:text-lg dark:text-[var(--color-text)]">
+            I have experience in IT support developer at PT.Megaputra. I provided IT support for many users, handling hardware maintenance, system installations, asset tracking, and cable upgrades. It helped me improve my technical and problem-solving skills.
           </p>
+
+          <p className="mb-8 text-md font-normal text-[var(--color-text)] md:text-lg dark:text-[var(--color-text)]">
+            Web Content Uploading at Gaotek Inc remotely. I also worked remotely with an international team to upload and manage website content during U.S. business hours. I stayed organized and completed tasks accurately, earning a certificate for my performance.
+          </p>
+
+
           <div className="grid grid-cols-2 gap-4">
             <img
               src="images/certificate/digital.png"
@@ -24,7 +33,7 @@ export function TimelineDemo() {
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
-            </div>
+          </div>
         </div>
       ),
     },
@@ -32,20 +41,21 @@ export function TimelineDemo() {
       title: "2024",
       content: (
         <div>
+         <h1 className="text-2xl mb-8 font-bold text-[var(--color-text)] dark:text-[var(--color-text)]">
+              Freelance Web Developer - Belle Design Studio <br></br> Programmer - PT. Exel Integrasi Solusindo 
+          </h1>
           <p
-            className="mb-8 text-md font-normal text-[var(--color-text)]md:text-lg dark:text[var(--color-text)]">
-            I usually run out of copy, but when I see content this big, I try to
-            integrate lorem ipsum.
+            className="mb-8 text-md font-normal text-[var(--color-text)] md:text-lg dark:text[var(--color-text)]">
+            I had the opportunity to work remotely as a freelance Web Developer at Belle Design Studio, where I gained my first experience using Three.js and working with 3D-related projects. And I received a satisfied testimonial from a client.
           </p>
           <p
-            className="mb-8 text-md font-normal text-[var(--color-text)]md:text-lg dark:text[var(--color-text)]">
-            Lorem ipsum is for people who are too lazy to write copy. But we are
-            not. Here are some more example of beautiful designs I built.
+            className="mb-8 text-md font-normal text-[var(--color-text)] md:text-lg dark:text[var(--color-text)]">
+          As a Programmer at PT. Exel Integrasi Solusindo, I worked on a CMS project for the client Djarum. I stayed organized and completed all tasks accurately, and received a certificate for my performance.
           </p>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://assets.aceternity.com/pro/hero-sections.png"
-              alt="hero template"
+              src=""
+              alt="Exel Certificate"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
@@ -70,115 +80,77 @@ export function TimelineDemo() {
       content: (
         <div>
           <p
-            className="mb-4 text-md font-normal text-[var(--color-text)]md:text-lg dark:text-[var(--color-text)]">
-            Deployed 5 new components on Aceternity today
+            className="mb-4 text-md font-normal text-[var(--color-text)] md:text-lg dark:text-[var(--color-text)]">
+            During my studies in Asia e University Malaysia, I gained internship experience and later worked on a contract basis while waiting for my graduation. At the same time, I continued doing freelance work. I eventually returned to Indonesia for visa cancellation.
+            <br></br>           
           </p>
           <div className="mb-8">
             <div
-              className="flex items-center gap-2 text-md text-neutral-700 md:text-lg dark:text-neutral-300">
-              ✅ Card grid component
+              className="flex items-center gap-2 text-md text-[var(--color-text)] md:text-lg dark:text-[var(--color-text)] font-bold">
+              ✅ Front End Developer - Lavu Tech Solution Sdn.Bhd
             </div>
             <div
-              className="flex items-center gap-2 text-md text-neutral-700 md:text-lg dark:text-neutral-300">
-              ✅ Startup template Aceternity
+              className="flex items-center gap-2 text-md text-[var(--color-text)] md:text-lg dark:text-[var(--color-text)] font-bold">
+              ✅ UI/UX Designer
             </div>
             <div
-              className="flex items-center gap-2 text-md text-neutral-700 md:text-lg dark:text-neutral-300">
-              ✅ Random file upload lol
-            </div>
-            <div
-              className="flex items-center gap-2 text-md text-neutral-700 md:text-lg dark:text-neutral-300">
-              ✅ Himesh Reshammiya Music CD
-            </div>
-            <div
-              className="flex items-center gap-2 text-md text-neutral-700 md:text-lg dark:text-neutral-300">
-              ✅ Salman Bhai Fan Club registrations open
+              className="flex items-center gap-2 text-md text-[var(--color-text)] md:text-lg dark:text-[var(--color-text)] font-bold">
+              ✅ Freelancer Wordpress
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://assets.aceternity.com/pro/hero-sections.png"
+              src="images/certificate/synergyxyz.png"
               alt="hero template"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
-            <img
-              src="https://assets.aceternity.com/features-section.png"
-              alt="feature template"
+              className="h-20 w-full rounded-lg shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
+                <img
+              src="images/certificate/sertif_AeU.jpg"
+              alt="hero template"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
-            <img
-              src="https://assets.aceternity.com/pro/bento-grids.png"
-              alt="bento template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
-            <img
-              src="https://assets.aceternity.com/cards.png"
-              alt="cards template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
+              className="h-20 w-full rounded-lg shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
           </div>
         </div>
       ),
     },
-     {
+    {
       title: "2019",
       content: (
         <div>
           <p
-            className="mb-4 text-md font-normal text-[var(--color-text)]md:text-lg dark:text-[var(--color-text)]">
-            Deployed 5 new components on Aceternity today
+            className="mb-4 text-md font-normal text-[var(--color-text)] md:text-lg dark:text-[var(--color-text)]">
+           I got into coding and started my career while studying at CEP CCIT, Faculty of Engineering, Universitas Indonesia. I took on freelance work, helped out with projects, did side gigs, and stayed active in tech communities and student events.
           </p>
           <div className="mb-8">
             <div
-              className="flex items-center gap-2 text-md text-neutral-700 md:text-lg dark:text-neutral-300">
-              ✅ Card grid component
+              className="flex items-center gap-2 text-md text-[var(--color-text)] md:text-lg dark:text-[var(--color-text)] font-bold">
+              ✅ Helper/Freelancer - Collage Assignment
+              
+            </div><br></br>
+            <div className="flex items-center gap-2 text-md text-[var(--color-text)] md:text-lg dark:text-[var(--color-text)] font-bold">Community :
+              <br></br>
+        
+              ✅ Induction Days CEP CCIT - Security Section
+               <br></br>
+              ✅ Android Community - Member
             </div>
-            <div
-              className="flex items-center gap-2 text-md text-neutral-700 md:text-lg dark:text-neutral-300">
-              ✅ Startup template Aceternity
-            </div>
-            <div
-              className="flex items-center gap-2 text-md text-neutral-700 md:text-lg dark:text-neutral-300">
-              ✅ Random file upload lol
-            </div>
-            <div
-              className="flex items-center gap-2 text-md text-neutral-700 md:text-lg dark:text-neutral-300">
-              ✅ Himesh Reshammiya Music CD
-            </div>
-            <div
-              className="flex items-center gap-2 text-md text-neutral-700 md:text-lg dark:text-neutral-300">
-              ✅ Salman Bhai Fan Club registrations open
-            </div>
+            
           </div>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://assets.aceternity.com/pro/hero-sections.png"
+              src="images/certificate/niit.png"
               alt="hero template"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
-            <img
-              src="https://assets.aceternity.com/features-section.png"
-              alt="feature template"
+              className="h-20 w-full rounded-lg shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
+               <img
+              src="images/certificate/toefl.png"
+              alt="hero template"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
-            <img
-              src="https://assets.aceternity.com/pro/bento-grids.png"
-              alt="bento template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
-            <img
-              src="https://assets.aceternity.com/cards.png"
-              alt="cards template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
+              className="h-20 w-full rounded-lg shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
           </div>
         </div>
       ),
