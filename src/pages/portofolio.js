1a185ad8003a0cbb03386a05a7d9664991e6b2f2
viathree <viathree01@gmail.com>
2025-05-28 10:22:41 +0700

    cv

--- src/pages/portofolio.js
+++ src/pages/portofolio.js
@@ -33,15 +33,15 @@ export default function Projects() {
 
       const categoryDescriptions = {
         "All":
-          "Explore our complete range of packaging projects across industries",
+          "",
         "Web Designer":
-          "Premium packaging for cosmetics, skincare, haircare, and personal care brands.",
+          ".",
         "UI/UX Designer":
-          "Custom packaging solutions for Support products, healthcare, and compliance needs.",
+          "",
         "Fullstack Developer ":
-          "High-volume packaging for food, beverages, and other fast-moving consumer goods, including retail and restaurant brands.",
+          "",
         "Support":
-          "Custom packaging solutions for Support products, healthcare, and compliance needs.",
+          "",
       };
 
   return (
@@ -49,10 +49,10 @@ export default function Projects() {
      
       <section className=" flex flex-col items-left justify-center md:px-16 lg:px-24 xl:px-44 pt-30 bg-[var(--backg)] text-[var(--color-text)]">
       <h1 className="text-3xl font-bold text-left my-2 text-[var(--color-primary)]">
-        Packaging Solutions for every industry
+       
         </h1>
         <h2 className="text-start text-justify text-[var(--color-text)]">
-        With decades of experience in packaging, Mega Putra partners with leading brands across a wide range of industries. Our packaging solutions are designed to meet the functional, regulatory, and aesthetic needs of each market — ensuring brand consistency and high-quality execution.
+       
         </h2>
          {/* Filter Tabs */}
          <p className="text-left text-md font-semibold text-[var(--color-text)] mt-2">
