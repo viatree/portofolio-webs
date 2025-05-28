2febcd619d955611a9783af5eff14d19fe638413
viathree <viathree01@gmail.com>
2025-05-28 17:01:51 +0700

    finish

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -106,7 +106,7 @@ export function TimelineDemo() {
               height={500}
               className="h-20 w-full rounded-lg shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
                 <img
-              src="images/certificate/sertif_AeU.jpg"
+              src="images/certificate/Sertif_AeU.jpg"
               alt="hero template"
               width={500}
               height={500}
@@ -140,7 +140,7 @@ export function TimelineDemo() {
           </div>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="images/certificate/niit.png"
+              src="images/certificate/Niit.png"
               alt="hero template"
               width={500}
               height={500}
